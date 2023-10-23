98679ffe4656a49257e749da54bc2dba5e91e5eb
Alexa123 <ntat01032002@gmail.com>
2023-10-24 02:10:48 +0700

    add dark-light mode

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,24 @@
-import { createTheme } from "@mui/material/styles";
-import { red } from '@mui/material/colors'
+import { teal, deepOrange, red, orange, cyan } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles"
 
-const theme = createTheme({
-  palette: {
-      primary: {
-        main: '#556cd6',
-      },
-      secondary: {
-        main: '#19857b',
-      },
-      error: {
-        main: red.A400
+  const theme = extendTheme({
+      colorSchemes: {
+        light: {
+          palette: {
+            primary: teal,
+            secondary: deepOrange
+          }
+        },
+        dark: {
+          palette: {
+            primary: cyan,
+            secondary: orange
+          }
+        }
       }
-  }
-})
+      // ...other properties
+  })
+
+
 
 export default theme
